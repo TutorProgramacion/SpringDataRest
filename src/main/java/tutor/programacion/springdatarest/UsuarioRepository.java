3ade38a87c0dd021a616e5eff5d082c6d7f1b076
carmelo <marin.carmelo.abrego@gmail.com>
2015-05-08 09:06:31 -0600

    Proyecto Spring Data Rest Final

--- src/main/java/tutor/programacion/springdatarest/UsuarioRepository.java
+++ src/main/java/tutor/programacion/springdatarest/UsuarioRepository.java
@@ -1,6 +1,6 @@
 package tutor.programacion.springdatarest;
 
-//import java.util.List;
+import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,6 +8,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "data", path = "user")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
 
-//    List<Usuario> findByNombre(@Param("nombre") String nombre);
-    Usuario findByNombre(@Param("nombre") String nombre);
+    List<Usuario> findByNombre(@Param("nombre") String nombre);
+    Usuario findByEmail(@Param("email") String email);
+
 }
